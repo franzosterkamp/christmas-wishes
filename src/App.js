e4489d8c990ca2f2ecc0f418c0f4eb64006701d0
Leon Machens <leon.machens@googlemail.com>
2019-11-28 10:58:46 +0000

    Add react-router (#3)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { ThemeProvider } from "emotion-theming";
 import defaultTheme from "./themes/defaultTheme";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BottomNavigation from "./components/BottomNavigation";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <div>App</div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div>App</div>
+          </Route>
+          <Route exact path="/new">
+            <div>New</div>
+          </Route>
+          <Route>
+            <div>404 Not found</div>
+          </Route>
+        </Switch>
+        <BottomNavigation />
+      </Router>
     </ThemeProvider>
   );
 }
